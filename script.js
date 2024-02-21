840493053b21c06626390b5cdcfb53081e69acc6
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-21 16:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    var select = document.getElementById("colorSelect");
+    var button = document.querySelector("input[type='button']");
+
+    button.addEventListener("click", function() {
+        var selectedOption = select.options[select.selectedIndex];
+        if (selectedOption) {
+            select.removeChild(selectedOption);
+        }
+    });
+});
